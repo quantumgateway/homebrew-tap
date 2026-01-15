6881d66a6265d2d5de87e3a0852b178160b589a0
Robert Augustinus <rob@opensolutions.nl>
2026-01-15 12:37:26 +0100

    dbmigrate v1.0.3

--- Formula/dbmigrate.rb
+++ Formula/dbmigrate.rb
@@ -1,14 +1,14 @@
 class Dbmigrate < Formula
   desc "Database migration tool for ClickHouse"
   homepage "https://github.com/quantumgateway/dbmigrate"
-  url "https://github.com/quantumgateway/dbmigrate/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "7d87ef7cd2c6c3a33c1565b7917addd4eb4910ab603c2221f4db0a5f239264e0"
+  url "https://github.com/quantumgateway/dbmigrate/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "26f04804bd7a3509a27dfff99667cd8163a6835dc0f75fd89bd42ca69ef60f3a"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=v1.0.2")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=v1.0.3")
   end
 
   test do
